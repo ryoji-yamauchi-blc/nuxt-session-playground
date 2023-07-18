7391e906609cd0e708574b871995b083b1fac335
ryoji-yamauchi-blc <ryoji.yamauchi.ut@ambl.co.jp>
2023-07-18 16:59:23 +0900

    reproduction

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@sidebase/nuxt-session"],
+  session: {
+    session: {
+      storageOptions: {
+        driver: "fs",
+        options: {
+          base: "./data",
+        },
+      },
+    },
+  },
+});
